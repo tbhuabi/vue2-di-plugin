b59c4cd27e1200e6ad60614a241b1666ad6b0da5
tanbo <tanbo@cmcm.com>
2021-06-04 18:33:06 +0800

    feat: 添加单元测试替换上下文支持

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,15 +3,37 @@ import { Injector, NullInjector, Provider, ReflectiveInjector } from '@tanbo/di'
 
 const DIInjectKey = Symbol('DIInjectKey') as InjectionKey<Injector>
 
+let replacedContextInjector: ReflectiveInjector = null;
+
+function createReplacedInjector(providers: Provider[] = []) {
+  replacedContextInjector.parentInjector = new ReflectiveInjector(new NullInjector(), providers);
+  provide(DIInjectKey, replacedContextInjector)
+  const injector = replacedContextInjector;
+  replacedContextInjector = null;
+  return injector;
+}
+
 export function useRootReflectiveInjector(providers: Provider[] = []) {
+  if (replacedContextInjector) {
+    return createReplacedInjector(providers)
+  }
   const contextInjector = new ReflectiveInjector(new NullInjector(), providers);
   provide(DIInjectKey, contextInjector)
   return contextInjector
 }
 
 export function useReflectiveInjector(providers: Provider[] = []) {
+  if (replacedContextInjector) {
+    return createReplacedInjector(providers)
+  }
   const parentInjector = inject(DIInjectKey) as Injector
   const contextInjector = new ReflectiveInjector(parentInjector, providers);
   provide(DIInjectKey, contextInjector)
   return contextInjector
+}
+
+export class TestBad {
+  static useReflectiveInjector(providers: Provider[]) {
+    replacedContextInjector = new ReflectiveInjector(null, providers)
+  }
 }
